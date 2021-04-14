94af282a9685f65bc5f60e38bdad93d69a261515
dimanil <dmytro.ilienko@datasentics.com>
2021-04-14 17:31:49 +0200

    Added functionality to display the whole feature table without specifying columns

--- src/featurestorebundle/feature/FeatureStore.py
+++ src/featurestorebundle/feature/FeatureStore.py
@@ -12,11 +12,14 @@ class FeatureStore:
         self.__feature_manager = feature_manager
         self.__spark = spark
 
-    def get(self, entity_name: str, feature_names: List[str]):
+    def get(self, entity_name: str, feature_names: List[str] = []):
         registered_feature_list = self.__feature_manager.get_features(entity_name)
         unregistered_features = set(feature_names) - set(registered_feature_list.get_names())
 
         if unregistered_features != set():
             raise Exception(f"Features {','.join(unregistered_features)} not registered for entity {entity_name}")
+
+        if feature_names == []:
+            feature_names = registered_feature_list
 
         return self.__feature_manager.get_values(entity_name, feature_names)
