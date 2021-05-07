934bce6d2c85137ccabf1a8de82dd1e4ef96f249
Karel Gudera <karel.gudera@datasentics.com>
2021-05-07 11:10:40 +0200

    Function for getting latest features

--- src/featurestorebundle/feature/FeatureStore.py
+++ src/featurestorebundle/feature/FeatureStore.py
@@ -1,5 +1,7 @@
 from typing import List
 from pyspark.sql import SparkSession
+from pyspark.sql import Window
+from pyspark.sql import functions as f
 from featurestorebundle.feature.FeatureManager import FeatureManager
 
 
@@ -22,3 +24,19 @@ class FeatureStore:
             raise Exception(f"Features {','.join(unregistered_features)} not registered for entity {entity_name}")
 
         return self.__feature_manager.get_values(entity_name, feature_names)
+
+    def get_latest(self, entity_name: str, feature_names: List[str] = None):
+        df = self.get(entity_name, feature_names)
+        id_column = df.columns[0]
+        time_column = df.columns[1]
+        features = df.columns[2:]
+
+        window = (
+            Window().partitionBy(id_column).orderBy(f.desc(time_column)).rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
+        )
+
+        return (
+            df.select(id_column, *[f.first(feature, ignorenulls=True).over(window).alias(feature) for feature in features])
+            .groupBy(id_column)
+            .agg(*[f.first(feature).alias(feature) for feature in features])
+        )
