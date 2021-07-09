c73ac95601e15ff8e93b31cbde337bf7cffc6f9b
Karel Gudera <karel.gudera@datasentics.com>
2021-07-09 22:33:44 +0200

    optimalization

--- src/featurestorebundle/delta/DeltaWriter.py
+++ src/featurestorebundle/delta/DeltaWriter.py
@@ -24,10 +24,29 @@ class DeltaWriter(FeaturesWriterInterface):
     def write_all(self, features_storage: FeaturesStorage):
         self.__table_preparer.prepare(features_storage.entity, features_storage.feature_list)
 
-        joined_df = features_storage.results[0]
+        join_batch_size = 10
+        batch_counter = 0
 
-        for df in features_storage.results[1:]:
-            joined_df = joined_df.join(df, on=[features_storage.entity.id_column, features_storage.entity.time_column], how="outer")
+        id_dataframes = [
+            df.select(features_storage.entity.id_column, features_storage.entity.time_column) for df in features_storage.results
+        ]
+
+        unique_ids_df = id_dataframes[0]
+
+        for df in id_dataframes[1:]:
+            unique_ids_df.union(df)
+
+        unique_ids_df = unique_ids_df.distinct()
+        joined_df = unique_ids_df.cache()
+
+        for df in features_storage.results:
+            batch_counter += 1
+
+            joined_df = joined_df.join(df, on=[features_storage.entity.id_column, features_storage.entity.time_column], how="left")
+
+            if batch_counter == join_batch_size:
+                joined_df = joined_df.persist()
+                batch_counter = 0
 
         self.__feature_data_merger.merge(
             features_storage.entity,
