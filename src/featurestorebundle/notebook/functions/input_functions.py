92314f2a26652277d4d3a44209699632adff0163
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2022-02-16 14:56:18 +0100

    Add add_timestamps

--- src/featurestorebundle/notebook/functions/input_functions.py
+++ src/featurestorebundle/notebook/functions/input_functions.py
@@ -1,19 +1,61 @@
-from typing import Optional, List
+from logging import Logger
+from datetime import datetime as dt
+from typing import Optional, List, Callable
 
+from pyspark.sql import DataFrame, functions as f
+
+from daipecore.widgets.Widgets import Widgets
 from daipecore.decorator.InputDecorator import InputDecorator
 from daipecore.function.input_decorator_function import input_decorator_function
 from injecta.container.ContainerInterface import ContainerInterface
 
 from featurestorebundle.entity.Entity import Entity
+from featurestorebundle.feature.FeatureStore import FeatureStore
 from featurestorebundle.notebook.WindowedDataFrame import WindowedDataFrame
 
 
 @input_decorator_function
-def make_windowed(input_decorator: InputDecorator, entity: Entity, time_column: str, custom_time_windows: Optional[List[str]] = None):
-    def wrapper(container: ContainerInterface):
+def make_windowed(
+    input_decorator: InputDecorator, entity: Entity, time_column: str, custom_time_windows: Optional[List[str]] = None
+) -> Callable[[ContainerInterface], WindowedDataFrame]:
+    def wrapper(container: ContainerInterface) -> WindowedDataFrame:
         default_time_windows = container.get_parameters().featurestorebundle.time_windows
         time_windows = default_time_windows if custom_time_windows is None else custom_time_windows
 
         return WindowedDataFrame(input_decorator.result, entity, time_column, time_windows)
+
+    return wrapper
+
+
+@input_decorator_function
+def add_timestamps(input_decorator: InputDecorator, entity: Entity) -> Callable[[ContainerInterface], DataFrame]:
+    def wrapper(container: ContainerInterface) -> DataFrame:
+        widgets: Widgets = container.get(Widgets)
+        logger: Logger = container.get("featurestorebundle.logger")
+
+        timestamp = widgets.get_value("timestamp")
+        target_name = widgets.get_value("target_name")
+
+        if target_name == "<no target>":
+            logger.info(f"No target was selected, adding `{entity.time_column}` with value `{timestamp}`")
+            return input_decorator.result.withColumn(entity.time_column, f.lit(timestamp).cast(entity.time_column_type))
+
+        feature_store: FeatureStore = container.get(FeatureStore)
+
+        time_shift = widgets.get_value("number_of_time_units")
+        target_date_from = dt.strptime(widgets.get_value("target_date_from"), "%Y%m%d")
+        target_date_to = dt.strptime(widgets.get_value("target_date_to"), "%Y%m%d")
+
+        logger.info(f"Loading targets for selected target={target_name}")
+        targets = feature_store.get_targets(
+            entity.name,
+            target_name,
+            target_date_from,
+            target_date_to,
+            time_shift,
+        )
+
+        logger.info("Joining targets with the input data")
+        return input_decorator.result.join(targets, on=[entity.id_column], how="inner")
 
     return wrapper
