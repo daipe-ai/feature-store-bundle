995c9963d0f7304f6b1a1a49d989bc2de65b32b0
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2022-03-01 14:00:12 +0100

    Add checking for time column type

--- src/featurestorebundle/notebook/functions/input_functions.py
+++ src/featurestorebundle/notebook/functions/input_functions.py
@@ -10,29 +10,46 @@ from featurestorebundle.notebook.WindowedDataFrame import WindowedDataFrame
 from featurestorebundle.notebook.services.TimestampAdder import TimestampAdder
 
 
+class WrongColumnTypeError(Exception):
+    pass
+
+
+def __check_time_column_type(df: DataFrame, time_column_name: str):
+    dtypes_dict = dict(df.dtypes)
+
+    if dtypes_dict[time_column_name] not in ["date", "timestamp"]:
+        raise WrongColumnTypeError(
+            f"Colum {time_column_name} is neither of DateType or of TimestampType. Please convert it or use another appropriate column."
+        )
+
+
 @input_decorator_function
 def make_windowed(
-    input_data: Union[InputDecorator, DataFrame], entity: Entity, time_column: str, custom_time_windows: Optional[List[str]] = None
+    input_data: Union[InputDecorator, DataFrame], entity: Entity, time_column_name: str, custom_time_windows: Optional[List[str]] = None
 ) -> Callable[[ContainerInterface], WindowedDataFrame]:
+    df = input_data.result if isinstance(input_data, InputDecorator) else input_data
+    __check_time_column_type(df, time_column_name)
+
     def wrapper(container: ContainerInterface) -> WindowedDataFrame:
         default_time_windows = container.get_parameters().featurestorebundle.time_windows
         time_windows = default_time_windows if custom_time_windows is None else custom_time_windows
 
-        df = input_data.result if isinstance(input_data, InputDecorator) else input_data
-        return WindowedDataFrame(df, entity, time_column, time_windows)
+        return WindowedDataFrame(df, entity, time_column_name, time_windows)
 
     return wrapper
 
 
 @input_decorator_function
 def with_timestamps(
-    input_data: Union[InputDecorator, DataFrame], entity: Entity, comparison_col_name: str, custom_time_windows: Optional[List[str]] = None
+    input_data: Union[InputDecorator, DataFrame], entity: Entity, time_column_name: str, custom_time_windows: Optional[List[str]] = None
 ) -> Callable[[ContainerInterface], DataFrame]:
+    df = input_data.result if isinstance(input_data, InputDecorator) else input_data
+    __check_time_column_type(df, time_column_name)
+
     def wrapper(container: ContainerInterface) -> DataFrame:
         timestamp_adder: TimestampAdder = container.get(TimestampAdder)
 
-        df = input_data.result if isinstance(input_data, InputDecorator) else input_data
-        return timestamp_adder.add(df, entity, comparison_col_name, custom_time_windows)
+        return timestamp_adder.add(df, entity, time_column_name, custom_time_windows)
 
     return wrapper
 
