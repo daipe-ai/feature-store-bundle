2cf3540869cab8d1c8246f8ec5a4dbc04196328b
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2022-03-02 10:23:06 +0100

    Better validation

--- src/featurestorebundle/feature/FeaturesValidator.py
+++ src/featurestorebundle/feature/FeaturesValidator.py
@@ -8,14 +8,10 @@ class FeaturesValidator:
     def validate(self, entity: Entity, features_data: DataFrame, feature_list: FeatureList):
         technical_cols = [entity.id_column, entity.time_column, get_rainbow_table_hash_column().name]
 
-        data_column_names = list(
-            map(
-                lambda field: field.name,
-                filter(lambda field: field.name not in technical_cols, features_data.schema.fields),
-            )
-        )
+        data_column_names = filter(lambda column: column not in technical_cols, features_data.columns)
 
-        feature_names = feature_list.get_names()
+        if len(set(features_data.columns)) != len(features_data.columns):
+            raise Exception("Dataframe contains duplicate columns")
 
-        if data_column_names != feature_names:
-            raise Exception(f"Dataframe columns of size ({len(data_column_names)}) != features matched of size ({len(feature_names)})")
+        if len(set(data_column_names)) != len(feature_list.get_names()):
+            raise Exception("Dataframe columns do not match declared features")
