31ec19d6e8ff245ba1c3a365fd8d41b99097bf35
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2022-03-24 17:02:14 +0100

    Add exists method to targets readers

--- src/featurestorebundle/delta/target/reader/DeltaTableTargetsReader.py
+++ src/featurestorebundle/delta/target/reader/DeltaTableTargetsReader.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
 from logging import Logger
 from featurestorebundle.db.TableNames import TableNames
+from featurestorebundle.delta.TableExistenceChecker import TableExistenceChecker
 from featurestorebundle.target.reader.TargetsReaderInterface import TargetsReaderInterface
 
 
@@ -11,10 +12,12 @@ class DeltaTableTargetsReader(TargetsReaderInterface):
         logger: Logger,
         table_names: TableNames,
         spark: SparkSession,
+        table_existence_checker: TableExistenceChecker,
     ):
         self.__logger = logger
         self.__spark = spark
         self.__table_names = table_names
+        self.__table_existence_checker = table_existence_checker
 
     def read(self, entity_name: str) -> DataFrame:
         full_table_name = self.__table_names.get_targets_full_table_name(entity_name)
@@ -27,3 +30,8 @@ class DeltaTableTargetsReader(TargetsReaderInterface):
         full_table_name = self.__table_names.get_targets_enum_full_table_name()
 
         return self.__spark.read.table(full_table_name)
+
+    def exists(self, entity_name: str) -> bool:
+        full_table_name = self.__table_names.get_targets_full_table_name(entity_name)
+
+        return self.__table_existence_checker.exists(full_table_name)
