2d662a455af20765496289f70aa9c17cc891c5b6
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2022-03-28 10:37:58 +0200

    Check existence of defaults

--- src/featurestorebundle/utils/errors.py
+++ src/featurestorebundle/utils/errors.py
@@ -2,6 +2,12 @@ class DateFormatError(Exception):
     pass
 
 
+class MissingWidgetDefaultError(Exception):
+    def __init__(self, widget_name: str):
+        message = f"Widget default for {widget_name} not found. Please set featurestorebundle.widgets.defaults.{widget_name} in config.yaml"
+        super().__init__(message)
+
+
 class MissingEntitiesError(Exception):
     pass
 
