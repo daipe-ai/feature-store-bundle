61c3fd6cf3abe56fe233637f2a692458172da7ed
Karel Gudera <karel.gudera@datasentics.com>
2022-04-16 11:26:41 +0200

    Frequency, start date and arguments are now configurable per notebook
    Example:
    
    parameters:
      featurestorebundle:
        orchestration:
          stage1:
            - notebook: '/path/to/ntb1'
              start_date: '2020-01-01'
              frequency: 'daily'
              arguments:
                foo: bar
                baz: inga

--- src/featurestorebundle/orchestration/NotebookTasksFactory.py
+++ src/featurestorebundle/orchestration/NotebookTasksFactory.py
@@ -1,4 +1,5 @@
 from typing import List
+from box import Box
 from pyspark.dbutils import DBUtils
 from featurestorebundle.orchestration.NotebookTask import NotebookTask
 
@@ -7,7 +8,17 @@ class NotebookTasksFactory:
     def __init__(self, dbutils: DBUtils):
         self.__dbutils = dbutils
 
-    def create(self, notebooks: List[str]) -> List[NotebookTask]:
-        parameters = dict(self.__dbutils.notebook.entry_point.getCurrentBindings())
+    def create(self, notebook_definitions: List[Box]) -> List[NotebookTask]:
+        notebook_arguments = dict(self.__dbutils.notebook.entry_point.getCurrentBindings())
+        notebook_tasks = []
 
-        return [NotebookTask(notebook_path, parameters=parameters) for notebook_path in notebooks]
+        for notebook_definition in notebook_definitions:
+            # for orchestration backwards compatibility
+            if isinstance(notebook_definition, str):
+                notebook_tasks.append(NotebookTask(notebook_definition, parameters=notebook_arguments))
+
+            else:
+                config_arguments = notebook_definition.arguments if "arguments" in notebook_definition else {}
+                notebook_tasks.append(NotebookTask(notebook_definition.notebook, parameters={**notebook_arguments, **config_arguments}))
+
+        return notebook_tasks
--- src/featurestorebundle/widgets/StageGetter.py
+++ src/featurestorebundle/widgets/StageGetter.py
@@ -1,17 +1,19 @@
-from typing import Dict, List
-
+from box import Box
 from daipecore.function.input_decorator_function import input_decorator_function
 from injecta.container.ContainerInterface import ContainerInterface
 from py4j.protocol import Py4JError
 from pyspark.dbutils import DBUtils
 
 from featurestorebundle.widgets.WidgetsFactory import WidgetsFactory
+from featurestorebundle.orchestration.NotebookDefinitionGetter import NotebookDefinitionGetter
 
 
 @input_decorator_function
 def get_stages():
-    def wrapper(container: ContainerInterface) -> Dict[str, List[str]]:
+    def wrapper(container: ContainerInterface) -> Box:
         dbutils: DBUtils = container.get(DBUtils)
+        notebook_definition_getter: NotebookDefinitionGetter = container.get(NotebookDefinitionGetter)
+
         try:
             notebooks_str = dbutils.widgets.get("notebooks")
         except Py4JError:
@@ -23,17 +25,19 @@ def get_stages():
         notebooks_list = notebooks_str.split(",")
         if WidgetsFactory.all_notebooks_placeholder in notebooks_list:
             raise Exception(
-                f"`{WidgetsFactory.all_notebooks_placeholder}` together with selected notebooks is not a valid option. Please select either `{WidgetsFactory.all_notebooks_placeholder}` only or a subset of notebooks"
+                f"`{WidgetsFactory.all_notebooks_placeholder}` together with selected notebooks is not a valid option. Please select "
+                f"either `{WidgetsFactory.all_notebooks_placeholder}` only or a subset of notebooks"
             )
 
-        stages = {}
-        for stage_notebook in notebooks_list:
-            stage, notebook = stage_notebook.split(":")
-            if stage not in stages:
-                stages[stage] = [notebook.lstrip()]
-            else:
-                stages[stage].append(notebook.lstrip())
-        return stages
+        stages = [stage.split(":")[0].strip() for stage in notebooks_list]
+        notebooks = [notebook.split(":")[1].strip() for notebook in notebooks_list]
+        stages_definition = {stage: [] for stage in stages}
+
+        for stage, notebook in zip(stages, notebooks):
+            notebook_definition = notebook_definition_getter.get(notebook)
+            stages_definition[stage].append(notebook_definition)
+
+        return Box(stages_definition)
 
     return wrapper
 
