e1182e01e4880cefbe406c424ac5666a4a0c1e0f
Karel Gudera <karel.gudera@datasentics.com>
2022-05-11 12:21:28 +0200

    Metadata schema evolution
    Metadata writer now automatically updates schema if there are new metadata columns

--- src/featurestorebundle/delta/metadata/writer/DeltaTableMetadataPreparer.py
+++ src/featurestorebundle/delta/metadata/writer/DeltaTableMetadataPreparer.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from featurestorebundle.db.TableNames import TableNames
 from featurestorebundle.delta.TableCreator import TableCreator
+from featurestorebundle.delta.TableSchemaMerger import TableSchemaMerger
 from featurestorebundle.delta.metadata.schema import get_metadata_schema, get_metadata_pk_columns
 
 
@@ -9,12 +10,15 @@ class DeltaTableMetadataPreparer:
         self,
         table_names: TableNames,
         table_creator: TableCreator,
+        table_schema_merger: TableSchemaMerger,
         spark: SparkSession,
     ):
         self.__table_names = table_names
         self.__table_creator = table_creator
+        self.__table_schema_merger = table_schema_merger
         self.__spark = spark
 
     def prepare(self, full_table_name: str, path: str):
         self.__spark.sql(f"CREATE DATABASE IF NOT EXISTS {self.__table_names.get_db_name()}")
         self.__table_creator.create_if_not_exists(full_table_name, path, get_metadata_schema(), get_metadata_pk_columns()[0].name)
+        self.__table_schema_merger.merge(full_table_name, get_metadata_schema())
