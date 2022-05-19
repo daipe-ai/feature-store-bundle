c754eeec8bc57eb73b5344e497dd265db2bb2546
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2022-05-19 12:14:38 +0200

    Add better incomplete rows error

--- src/featurestorebundle/utils/errors.py
+++ src/featurestorebundle/utils/errors.py
@@ -2,6 +2,10 @@ class DateFormatError(Exception):
     pass
 
 
+class IncompleteRowsError(Exception):
+    pass
+
+
 class MissingWidgetDefaultError(Exception):
     def __init__(self, widget_name: str):
         message = f"Widget default for {widget_name} not found. Please set featurestorebundle.widgets.defaults.{widget_name} in config.yaml"
