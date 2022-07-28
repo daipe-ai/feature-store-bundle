107657c34d0bc20eaa27275f06ca801610aef663
Karel Gudera <karel.gudera@datasentics.com>
2022-07-28 19:40:11 +0200

    Databricks Feature Store backend now respects path from config

--- src/featurestorebundle/delta/PathCreator.py
+++ src/featurestorebundle/delta/PathCreator.py
@@ -1,8 +1,9 @@
-from typing import Union, Optional
+from typing import List, Union, Optional
 from logging import Logger
 from pyspark.sql.types import StructType
 from featurestorebundle.delta.PathExistenceChecker import PathExistenceChecker
 from featurestorebundle.delta.EmptyDataFrameCreator import EmptyDataFrameCreator
+from featurestorebundle.delta.TablePropertiesSetter import TablePropertiesSetter
 
 
 class PathCreator:
@@ -11,12 +12,14 @@ class PathCreator:
         logger: Logger,
         path_existence_checker: PathExistenceChecker,
         empty_dataframe_creator: EmptyDataFrameCreator,
+        table_properties_setter: TablePropertiesSetter,
     ):
         self.__logger = logger
         self.__path_existence_checker = path_existence_checker
         self.__empty_dataframe_creator = empty_dataframe_creator
+        self.__table_properties_setter = table_properties_setter
 
-    def create_if_not_exists(self, path: str, schema: StructType, partition_by: Optional[Union[str, list]] = None):
+    def create_if_not_exists(self, path: str, schema: StructType, partition_by: Optional[Union[str, List[str]]] = None):
         partition_by = partition_by or []
         partition_by = [partition_by] if isinstance(partition_by, str) else partition_by
 
@@ -30,4 +33,16 @@ class PathCreator:
 
         df.write.partitionBy(*partition_by).format("delta").save(path)
 
+        self.__set_delta_name_column_mapping_mode(path)
+
         self.__logger.info("Delta table successfully created")
+
+    def __set_delta_name_column_mapping_mode(self, path: str):
+        self.__table_properties_setter.set_properties(
+            table_identifier=f"delta.`{path}`",
+            properties={
+                "delta.minReaderVersion": "2",
+                "delta.minWriterVersion": "5",
+                "delta.columnMapping.mode": "name",
+            },
+        )
