2f95edf7e22c07bbb489a4d5f641fafdcb0aa3e6
Karel Gudera <karel.gudera@datasentics.com>
2022-08-03 14:33:48 +0200

    Read databricks feature store with plain spark

--- src/featurestorebundle/databricks/feature/reader/DatabricksFeatureStoreReader.py
+++ src/featurestorebundle/databricks/feature/reader/DatabricksFeatureStoreReader.py
@@ -1,27 +1,18 @@
 from logging import Logger
+from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
 from featurestorebundle.feature.reader.FeaturesReaderInterface import FeaturesReaderInterface
 
 
 class DatabricksFeatureStoreReader(FeaturesReaderInterface):
-    def __init__(self, logger: Logger):
+    def __init__(self, logger: Logger, spark: SparkSession):
         self.__logger = logger
+        self.__spark = spark
 
     def read(self, location: str) -> DataFrame:
         self.__logger.info(f"Reading features from Databricks Feature Store table {location}")
 
-        return self.__read_from_databricks_feature_store(location)
+        return self.__spark.read.table(location)
 
     def get_backend(self) -> str:
         return "databricks"
-
-    def __read_from_databricks_feature_store(self, full_table_name: str) -> DataFrame:
-        try:
-            from databricks import feature_store  # pyre-ignore[21] pylint: disable=import-outside-toplevel
-
-        except ImportError as exception:
-            raise Exception("Cannot import Databricks Feature Store, you need to use ML cluster with DBR 8.3+") from exception
-
-        dbx_feature_store_client = feature_store.FeatureStoreClient()
-
-        return dbx_feature_store_client.read_table(full_table_name)
