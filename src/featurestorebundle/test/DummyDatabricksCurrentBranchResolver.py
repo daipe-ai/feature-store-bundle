6a83b5f5c61a4374a92a068b73ea0778be2e027e
Karel Gudera <karel.gudera@datasentics.com>
2022-08-17 12:30:29 +0200

    Improved placeholders replacement

--- src/featurestorebundle/test/DummyDatabricksCurrentBranchResolver.py
+++
@@ -1,9 +0,0 @@
-from featurestorebundle.databricks.DatabricksCurrentBranchResolver import DatabricksCurrentBranchResolver
-
-
-class DummyDatabricksCurrentBranchResolver(DatabricksCurrentBranchResolver):
-    def __init__(self):  # noqa # pylint: disable=super-init-not-called
-        pass
-
-    def resolve(self) -> str:
-        return "main"
