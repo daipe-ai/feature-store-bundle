875be97d34f7c9b1e1d8f23647aa4c73fd731bf0
Karel Gudera <karel.gudera@gmail.com>
2023-01-02 14:34:57 +0100

    Fixed databricks feature store deleter

--- src/featurestorebundle/databricks/feature/deleter/DatabricksFeatureStoreDeleter.py
+++ src/featurestorebundle/databricks/feature/deleter/DatabricksFeatureStoreDeleter.py
@@ -1,5 +1,3 @@
-import random
-import string
 from typing import List
 from logging import Logger
 from pyspark.sql import SparkSession
@@ -33,19 +31,16 @@ class DatabricksFeatureStoreDeleter(FeaturesDeleterInterface):
     def delete(self, features: List[str]):
         entity = self.__entity_getter.get()
         full_table_name = self.__table_names.get_features_full_table_name(entity.name)
-        random_string = "".join(random.choice(string.ascii_lowercase) for _ in range(6))
-        temp_table_name = full_table_name + random_string
-        delete_query = self.__delete_columns_query_builder.build_delete_columns_query(temp_table_name, features)
+        path = self.__table_names.get_features_path(entity.name)
+        delete_query = self.__delete_columns_query_builder.build_delete_columns_query(full_table_name, features)
 
         self.__logger.info(f"Deleting features {', '.join(features)}")
 
-        self.__spark.sql(f"ALTER TABLE {full_table_name} RENAME TO {temp_table_name}")
+        self.__spark.sql(delete_query)
 
         self.__feature_store_client.drop_table(full_table_name)
 
-        self.__spark.sql(delete_query)
-
-        self.__spark.sql(f"ALTER TABLE {temp_table_name} RENAME TO {full_table_name}")
+        self.__spark.sql(f"CREATE TABLE {full_table_name} USING DELTA LOCATION '{path}'")
 
         self.__feature_store_client.register_table(delta_table=full_table_name, primary_keys=entity.get_primary_key())
 
