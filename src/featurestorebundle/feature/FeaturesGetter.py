95f20a89e2bfcb1be245473b164c5db1fd90820e
Karel Gudera <karel.gudera@gmail.com>
2023-03-14 12:18:37 +0100

    FeaturesGetter now returns features with respect to timestamp in widgets

--- src/featurestorebundle/feature/FeaturesGetter.py
+++ src/featurestorebundle/feature/FeaturesGetter.py
@@ -1,54 +1,55 @@
 from logging import Logger
 from typing import List, Optional
-
-from daipecore.widgets.Widgets import Widgets
+from datetime import timedelta
 
 from featurestorebundle.utils.DateParser import DateParser
 from featurestorebundle.entity.EntityGetter import EntityGetter
 from featurestorebundle.feature.FeatureStore import FeatureStore
-from featurestorebundle.widgets.WidgetNames import WidgetNames
+from featurestorebundle.widgets.WidgetsGetter import WidgetsGetter
+from featurestorebundle.notebook.functions.time_windows import parse_time_window
 
 
 class FeaturesGetter:
     def __init__(
         self,
+        timestamp_shift: str,
         logger: Logger,
         date_parser: DateParser,
         entity_getter: EntityGetter,
         feature_store: FeatureStore,
-        widget_names: WidgetNames,
-        widgets: Widgets,
+        widgets_getter: WidgetsGetter,
     ):
+        self.__timestamp_shift = parse_time_window(timestamp_shift)
         self.__logger = logger
         self.__date_parser = date_parser
         self.__entity_getter = entity_getter
         self.__feature_store = feature_store
-        self.__widget_names = widget_names
-        self.__widgets = widgets
+        self.__widgets_getter = widgets_getter
 
     def get_features(self, feature_names: Optional[List[str]] = None):
         feature_names = [] if feature_names is None else feature_names
-        target_name = self.__widgets.get_value(self.__widget_names.target_name)
+        target_name = self.__widgets_getter.get_target()
 
         return (
             self.__get_latest_features(feature_names)
-            if target_name == self.__widget_names.no_targets_placeholder
+            if target_name == self.__widgets_getter.timestamp_exists()
             else self.__get_features_for_target(feature_names)
         )
 
     def __get_latest_features(self, feature_names: List[str]):
         entity = self.__entity_getter.get()
+        timestamp = self.__date_parser.parse_date(self.__widgets_getter.get_timestamp()) + timedelta(**self.__timestamp_shift)
 
         self.__logger.info(f"Loading latest features for entity '{entity.name}'")
 
-        return self.__feature_store.get_latest(entity.name, features=feature_names, skip_incomplete_rows=True)
+        return self.__feature_store.get_latest(entity.name, features=feature_names, timestamp=timestamp, skip_incomplete_rows=True)
 
     def __get_features_for_target(self, feature_names: List[str]):
         entity = self.__entity_getter.get()
-        target_name = self.__widgets.get_value(self.__widget_names.target_name)
-        target_date_from = self.__date_parser.parse_date(self.__widgets.get_value(self.__widget_names.target_date_from_name))
-        target_date_to = self.__date_parser.parse_date(self.__widgets.get_value(self.__widget_names.target_date_to_name))
-        target_time_shift = self.__widgets.get_value(self.__widget_names.target_time_shift)
+        target_name = self.__widgets_getter.get_target()
+        target_date_from = self.__date_parser.parse_date(self.__widgets_getter.get_target_date_from())
+        target_date_to = self.__date_parser.parse_date(self.__widgets_getter.get_target_date_to())
+        target_time_shift = self.__widgets_getter.get_target_time_shift()
 
         self.__logger.info(
             f"Getting entity '{entity.name}' features for target '{target_name}', from '{target_date_from}' to '{target_date_to}' "
